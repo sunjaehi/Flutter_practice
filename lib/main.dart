7fa8ac3fc48705db7430496d66126aee13d2504c
Lee Sunjae <sunjae7944@naver.com>
2024-07-28 21:07:04 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Image.asset('KakaoTalk_20240727_225507249.jpg')
+    );
+  }
+}
