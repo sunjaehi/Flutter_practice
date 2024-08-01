ede942a54960136906e97f60ba282f1269a3416b
Lee Sunjae <sunjae7944@naver.com>
2024-08-01 22:12:33 +0900

    feat : Flexible()

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('앱')),
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            width: double.infinity, height: 50, color: Colors.blue,
+        appBar: AppBar(),
+        body: Container(
+          height: 100,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image.asset('KakaoTalk_20240727_225507249.jpg', width: 200, height: 150,),
+              Container(
+                width: 300,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('숭이짤',style: TextStyle(fontSize: 15),),
+                    Text('귀야워ㅠ'),
+                    Text('사진'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4'),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-        ),
+        )
       ),
     );
 
