a41522f101c4a518f7d80c25112b63743b2490ee
Lee Sunjae <sunjae7944@naver.com>
2024-08-03 17:00:07 +0900

    feat : ListView()

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          height: 100,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('KakaoTalk_20240727_225507249.jpg', width: 200, height: 150,),
-              Container(
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('숭이짤',style: TextStyle(fontSize: 15),),
-                    Text('귀야워ㅠ'),
-                    Text('사진'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4'),
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+        body: ListView(
+          children : [
+            Text('hello'),
+          ]
         )
+
       ),
     );
 
   }
 }
+
