aaad185a70074c8197723cd4721dacb7b4b451ae
Lee Sunjae <sunjae7944@naver.com>
2024-08-21 20:43:03 +0900

    feat

---
+++ lib/src/binding/init_binding.dart
@@ -0,0 +1,9 @@
+import 'package:code_basic/src/controller/bottom_nav_controller.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class InitBinding extends Bindings{
+  @override
+  void dependencies() {
+    Get.put(BottomNavController(), permanent: true);
+  }
+}
