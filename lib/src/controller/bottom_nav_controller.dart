6a685d4a71605b96dfb0d4f49d637417c65ba9ce
Lee Sunjae <sunjae7944@naver.com>
2024-08-25 22:16:38 +0900

    feat : pageIndex 수정

--- lib/src/controller/bottom_nav_controller.dart
+++ lib/src/controller/bottom_nav_controller.dart
@@ -1,10 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+
+import '../pages/upload.dart';
+
+enum PageName {HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE}
 
 class BottomNavController extends GetxController {
   RxInt pageIndex = 0.obs;
+  List<int> bottomHistory = [0];
 
-
-  void changeBottomNav(int value) {
+  void changeBottomNav(int value,{bool hasGesture = true}) {
+    var page = PageName.values[value];
+    switch (page) {
+      case PageName.UPLOAD:
+        Get.to(() => const Upload());
+        break;
+      case PageName.HOME:
+      case PageName.SEARCH:
+      case PageName.ACTIVITY:
+      case PageName.MYPAGE:
+        _changePage(value, hasGesture: hasGesture);
+        break;
+    }
     pageIndex(value);
+  }
+  void _changePage(int value, {bool hasGesture = true}) {
+    pageIndex(value);
+    if (!hasGesture) return;
+    if(!bottomHistory.contains(value)) {
+      bottomHistory.add(value);
+    }
+    bottomHistory.add(value);
+    print(bottomHistory);
+  }
+
+  Future<bool> willPopAction() async {
+    if(bottomHistory.length==1) {
+      print('exit!');
+      return true;
+    } else {
+      print('goto before page!');
+      bottomHistory.removeLast();
+      var index = bottomHistory.last;
+      changeBottomNav(index, hasGesture: false);
+      return false;
+    }
+
   }
 }
