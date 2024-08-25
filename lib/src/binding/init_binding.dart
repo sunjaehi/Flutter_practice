42f0d8fdd7d4479af83a29902e18a3ada1b3bae9
Lee Sunjae <sunjae7944@naver.com>
2024-08-25 23:17:25 +0900

    feat : 패키지 추가

--- lib/src/binding/init_binding.dart
+++ lib/src/binding/init_binding.dart
@@ -1,4 +1,5 @@
 import 'package:code_basic/src/controller/bottom_nav_controller.dart';
+import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
 class InitBinding extends Bindings{
