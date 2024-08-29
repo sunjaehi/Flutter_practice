d8d4ed3fa8389998a07f344bf12dee29bb4627e2
Lee Sunjae <sunjae7944@naver.com>
2024-08-29 15:18:02 +0900

    feat : page 분리

--- lib/src/controller/bottom_nav_controller.dart
+++ lib/src/controller/bottom_nav_controller.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../components/message_popup.dart';
 import '../pages/upload.dart';
 
 enum PageName {HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE}
@@ -27,19 +31,31 @@ class BottomNavController extends GetxController {
   void _changePage(int value, {bool hasGesture = true}) {
     pageIndex(value);
     if (!hasGesture) return;
-    if(!bottomHistory.contains(value)) {
-      bottomHistory.add(value);
+    if (bottomHistory.last != value) {
+      bottomHistory.add(value);  //페이지 누적
+      print(bottomHistory);
     }
-    bottomHistory.add(value);
-    print(bottomHistory);
+    // if(!bottomHistory.contains(value)) {
+    //   bottomHistory.remove(value);
+    // }
+
   }
 
   Future<bool> willPopAction() async {
-    if(bottomHistory.length==1) {
-      print('exit!');
+    if(bottomHistory.length == 1) {
+      showDialog(
+          context: Get.context!,
+          builder: (context) => MessagePopup(
+                message: '종료하시겠습니까?',
+                okCallback: () {
+                  exit(0);
+                },
+                cancelCallback: Get.back,
+                title: '시스템',
+
+      ));
       return true;
     } else {
-      print('goto before page!');
       bottomHistory.removeLast();
       var index = bottomHistory.last;
       changeBottomNav(index, hasGesture: false);
