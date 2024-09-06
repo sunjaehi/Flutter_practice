7dd1d606f18aca6fc3b5accd658dce778cfb5d31
Lee Sunjae <sunjae7944@naver.com>
2024-09-07 00:11:58 +0900

    feat : 검색창 추가

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -3,35 +3,50 @@ import 'package:flutter/material.dart';
 class Search extends StatelessWidget {
   const Search({super.key});
 
-  Widget _appbar() {  //10분
+  Widget _appbar() {
     return Row(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(6),
-            color: const Color(0xffefefef),
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            margin: const EdgeInsets.only(left: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: const Color(0xffefefef),
+            ),
+            child: Row(children: [
+              Icon(Icons.search),
+              Text(
+                '검색',
+                style: TextStyle(fontSize: 15, color: Color(0xff838383),
+                ),
+              )
+            ],),
           ),
-          child: Row(children: [
-            Icon(Icons.search),
-            Text(
-              '검색',
-              style: TextStyle(fontSize: 15, color: Color(0xff838383),
-              ),
-            )
-          ],),
-        )
+        ),
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Icon(Icons.location_pin),
+        ),
       ],
     );
   }
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: ,
+    )
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          _appbar(),
-          // _body(),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            _appbar(),
+            Expanded(child: _body()),
+          ],
+        ),
       ),
     );
   }
