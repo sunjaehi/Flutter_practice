eb8bb7ab10a7cf71929254239869ed3266c5f35e
Lee Sunjae <sunjae7944@naver.com>
2024-09-08 00:34:57 +0900

    feat : 검색창 body 추가

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -1,8 +1,35 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Search extends StatelessWidget {
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:quiver/iterables.dart';
+
+class Search extends StatefulWidget {
   const Search({super.key});
 
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+
+  List<List<int>> groupBox = [[], [], []];
+  List<int> groupIndex = [0, 0, 0];
+
+  @override
+  void initState() {
+    super.initState();
+    for(var i=0; i<100; i++) {
+      var gi = groupIndex.indexOf(min<int>(groupIndex)!);
+      var size = 1;
+      if(gi!=1) {
+        size = Random().nextInt(100) % 2 == 0 ? 1 : 2;
+      }
+      groupBox[gi].add(size);
+    }
+  }
   Widget _appbar() {
     return Row(
       children: [
@@ -34,9 +61,50 @@ class Search extends StatelessWidget {
 
   Widget _body() {
     return SingleChildScrollView(
-      child: ,
-    )
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: List.generate(
+            groupBox.length,
+            (index) => Expanded(
+                child: Column(
+                  children: List.generate(
+                    groupBox[index].length,
+                        (jndex) => Container(
+                          height: Get.width*0.33*groupBox[index][jndex],
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white),
+                              color: Colors.primaries[
+                                  Random().nextInt(Colors.primaries.length)]),
+                        child :CachedNetworkImage(imageUrl: 'https://i.namu.wiki/i/7lKZi8BygJi_mxfoekHKJ0j8GxIn5cKkI50El7rDakNKhpuF3MdIBeuToNW4YOfYjzKloe1q0gdnbyM2bvB1NGU44ht49S5jIEtys6qKhDEP9VH5VI_KGojjEg6CRp-5XvPKS17z9jsSV9qDHlt9zg.webp',fit: BoxFit.cover,
+                        ),
+                      ),
+                    ).toList(),
+                  ),
+                ),)),
+        // children: [
+        //   Expanded(
+        //     child: Column(
+        //       children: [
+        //         Container(
+        //           height: 140,
+        //           color: Colors.red,
+        //         ),
+        //     ],),
+        //   ),
+        //   Expanded(
+        //     child: Column(children: [
+        //       Container(height: 140,color: Colors.blue,)
+        //     ],),
+        //   ),
+        //   Expanded(
+        //     child: Column(children: [
+        //       Container(height: 140,color: Colors.grey,)
+        //     ],),
+        //   ),
+        // ],
+      );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
