f0864a459379f8d3c469086da576d60962b76fa1
Lee Sunjae <sunjae7944@naver.com>
2024-09-09 00:07:17 +0900

    feat : searchFocus 파일 추가

---
+++ lib/src/pages/search/search_focus.dart
@@ -0,0 +1,90 @@
+import 'package:code_basic/src/components/image_data.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SearchFocus extends StatefulWidget {
+  const SearchFocus({super.key});
+
+  @override
+  State<SearchFocus> createState() => _SearchFocusState();
+}
+
+class _SearchFocusState extends State<SearchFocus> with TickerProviderStateMixin{
+
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 5, vsync: this);
+  }
+
+  Widget _tabMenuOne(String menu) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 15.0),
+      child: Text(
+        menu,
+        style: TextStyle(fontSize: 15, color: Colors.black),
+      ),
+    );
+  }
+  PreferredSizeWidget _tabMenu() {
+    return PreferredSize(
+      child: Container(
+        height: AppBar().preferredSize.height,
+        width: Size.infinite.width,
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Color(0xffe4e4e4)))),
+        child: TabBar(controller: tabController,
+            indicatorColor: Colors.black,
+            tabs:  [
+              _tabMenuOne('인기'),
+              _tabMenuOne('계정'),
+              _tabMenuOne('오디오'),
+              _tabMenuOne('태그'),
+              _tabMenuOne('장소'),
+            ],
+        ),
+      ),
+      preferredSize: Size.fromHeight(AppBar().preferredSize.height),
+    );
+  }
+
+  // Widget _body() {
+  //   //21분
+  // }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: Get.back,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(
+              IconsPath.backBtnIcon,
+            ),
+          ),
+        ),
+      titleSpacing: 0,
+      title: Container(
+        margin: const EdgeInsets.only(right: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: const Color(0xffefefef),
+        ),
+        child: const TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: '검색',
+            contentPadding: EdgeInsets.only(left: 15, top: 7, bottom: 7),
+            isDense: true,
+          ),
+        ),
+      ),
+        bottom: _tabMenu(),
+      ),
+      // body: _body(),
+    );
+  }
+}
