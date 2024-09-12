92f171abf7917b9b34daefeeab685d0649cbaeb9
Lee Sunjae <sunjae7944@naver.com>
2024-09-12 23:40:14 +0900

    feat : GestureDetector

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:code_basic/src/pages/search/search_focus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -34,21 +35,28 @@ class _SearchState extends State<Search> {
     return Row(
       children: [
         Expanded(
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            margin: const EdgeInsets.only(left: 15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(6),
-              color: const Color(0xffefefef),
+          child: GestureDetector(
+            onTap: () {
+              // Get.to(SearchFocus());
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SearchFocus()));
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              margin: const EdgeInsets.only(left: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: const Color(0xffefefef),
+              ),
+              child: Row(children: [
+                Icon(Icons.search),
+                Text(
+                  '검색',
+                  style: TextStyle(fontSize: 15, color: Color(0xff838383),
+                  ),
+                )
+              ],),
             ),
-            child: Row(children: [
-              Icon(Icons.search),
-              Text(
-                '검색',
-                style: TextStyle(fontSize: 15, color: Color(0xff838383),
-                ),
-              )
-            ],),
           ),
         ),
         Padding(
