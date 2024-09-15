f5cadd65fd8dac743654ce818e84ac2169531940
Lee Sunjae <sunjae7944@naver.com>
2024-09-16 01:19:41 +0900

    feat : upload파일 Widget 추가

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -13,9 +13,71 @@ class Upload extends StatelessWidget {
     );
   }
 
-  // Widget _header() { //10분
-  //
-  // }
+  Widget _header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(children: [
+              Text('갤러리', style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+              ),
+              Icon(Icons.arrow_drop_down),
+            ],),
+          ),
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical:5, horizontal: 10),
+                decoration: BoxDecoration(color: const Color(0xff808080),borderRadius: BorderRadius.circular(30)),
+                child: Row(children: [
+                ImageData(IconsPath.imageSelectIcon),
+                const SizedBox(width: 7),
+                const Text(
+                  '여러 항목 선택',
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                )
+              ],),
+              ),
+              const SizedBox(width: 5),
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xff808080),
+                ),
+                child: ImageData(IconsPath.cameraIcon),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _imageSelectList() {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(), //스크롤 X
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount:4, //가로 이미지 개수
+        childAspectRatio: 1,
+        mainAxisSpacing:1,
+        crossAxisSpacing: 1,
+      ),
+      itemCount: 100,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          color: Colors.red,
+        );
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,8 +114,8 @@ class Upload extends StatelessWidget {
         child: Column(
           children: [
             _imagePreview(),
-            // _header(),
-            // _imageSelectList(),
+            _header(),
+            _imageSelectList(),
           ],
         ),
       ),
