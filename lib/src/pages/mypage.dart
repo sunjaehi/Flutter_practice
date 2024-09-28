16852584fe88b517ba6a906a2268ced3298c871b
Lee Sunjae <sunjae7944@naver.com>
2024-09-29 00:49:25 +0900

    feat : menu, information 추가

--- lib/src/pages/mypage.dart
+++ lib/src/pages/mypage.dart
@@ -26,11 +26,12 @@ class MyPage extends StatelessWidget {
   }
 
   Widget _information() {
-    return Column(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
+         Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               AvatarWidget(
@@ -39,12 +40,98 @@ class MyPage extends StatelessWidget {
                   'https://blog.kakaocdn.net/dn/0mySg/btqCUccOGVk/nQ68nZiNKoIEGNJkooELF1/img.jpg',
                 size: 80,
               ),
-              _statisticsOne('Post',15),
-              _statisticsOne('Followers',11),
-              _statisticsOne('Following',3),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(child: _statisticsOne('Post',15)),
+                    Expanded(child: _statisticsOne('Followers',11)),
+                    Expanded(child: _statisticsOne('Following',3)),
+                  ],
+                ),
+              )
             ],
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          '안녕하세요 hi입니다.',
+          style: TextStyle(
+            fontSize: 13,
+            color: Colors.black,
           ),
         )
+      ],
+      ),
+    );
+  }
+
+  Widget _menu(){
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+      child: Row(
+        children: [
+          Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 7),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(3),
+                  border: Border.all(
+                      color: Color(0xffdedede),
+                  ),
+                ),
+            child: const Text('Edit profile',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          ),
+          SizedBox(width: 8),
+          Container(
+            padding: const EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3),
+              border: Border.all(
+                color: const Color(0xffdedede),
+              ),
+              color: const Color(0xffefefef),
+            ),
+            child: ImageData(IconsPath.addFriend),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _discoverPeople() {
+    return Column(
+      children: [ //27분
+        Row(
+          children: const [
+            Text('Discover People',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              Container(
+                  width: 100,
+                  height: 150,
+                  color: Colors.red
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
@@ -85,7 +172,11 @@ class MyPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            _information(),
+            _menu(),
+            _discoverPeople(),
+          ],
         ),
       ),
     );
