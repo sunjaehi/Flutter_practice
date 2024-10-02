81151b811fdf4ccd6db1f6459e193bd22843c54f
Lee Sunjae <sunjae7944@naver.com>
2024-10-02 18:15:29 +0900

    feat : Widget 변경

--- lib/src/pages/mypage.dart
+++ lib/src/pages/mypage.dart
@@ -1,11 +1,23 @@
 import 'package:code_basic/src/components/avatar_widget.dart';
+import 'package:code_basic/src/components/user_card.dart';
 import 'package:flutter/material.dart';
 
 import '../components/image_data.dart';
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
   const MyPage({super.key});
 
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> with TickerProviderStateMixin{
+  late TabController tabController;
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
   Widget _statisticsOne(String title, int value) {
     return Column(
       children: [
@@ -108,31 +120,77 @@ class MyPage extends StatelessWidget {
 
   Widget _discoverPeople() {
     return Column(
-      children: [ //27분
-        Row(
-          children: const [
-            Text('Discover People',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-                color: Colors.black,
-              ),
-            )
-          ],
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
           child: Row(
-            children: [
-              Container(
-                  width: 100,
-                  height: 150,
-                  color: Colors.red
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text('Discover People',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+              Text('See All',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ],
           ),
         ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            children: List.generate(10, (index) => UserCard(
+                userId: 'hi$index',
+                description : 'hie$index님이 팔로우합니다.',
+            ),
+            ).toList(),
+          ),
+        ),
       ],
+    );
+  }
+
+  Widget _tabMenu() {
+    return TabBar(
+      controller: tabController,
+        indicatorColor: Colors.black,
+        indicatorWeight: 1,
+        tabs: [
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: ImageData(IconsPath.gridViewOn),
+      ),
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: ImageData(IconsPath.myTagImageOff),
+      ),
+    ]);
+  }
+  
+  Widget _tabView() {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: 100,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      childAspectRatio: 1,
+      mainAxisSpacing: 1,
+      crossAxisSpacing: 1,
+    ),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.grey,
+          );
+        }
     );
   }
 
@@ -176,6 +234,9 @@ class MyPage extends StatelessWidget {
             _information(),
             _menu(),
             _discoverPeople(),
+            const SizedBox(height: 20),
+            _tabMenu(),
+            _tabView(),
           ],
         ),
       ),
